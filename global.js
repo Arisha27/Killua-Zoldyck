785480caa2454560e2ddd1df85c00c09ad90a88d
Arisha <105481786+Arisha27@users.noreply.github.com>
2022-08-02 20:06:52 +0500

    Update global.js

--- global.js
+++ global.js
@@ -8,12 +8,12 @@ global._group = JSON.parse(fs.readFileSync("./database/group.json"))
 global.mess = (type, m) => {
     let msg = {
         wait: 'Wait, in progress',
-        owner: 'Perintah ini hanya dapat digunakan oleh Owner!',
-        premium: 'Perintah ini hanya dapat digunakan oleh Premium!',
-        group: 'Perintah ini hanya dapat digunakan di group!',
-        private: 'Perintah ini hanya dapat digunakan di private chat!',
-        admin: 'Perintah ini hanya dapat digunakan oleh admin group!',
-        botAdmin: 'Bot bukan admin, tidak dapat mengakses fitur tersebut',
+        owner: 'Owner only!',
+        premium: 'Premium!',
+        group: 'group only!',
+        private: 'private chat!',
+        admin: ' admin only of group!',
+        botAdmin: 'Bot isnt admin, make it',
         bot: 'Fitur ini hanya dapat diakses oleh Bot',
         dead: 'Fitur ini sedang dimatikan!',
         media: 'Reply media',
