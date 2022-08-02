ef89a0babaebbc24c3f482d4744c5aae1e0af4ae
Arisha <105481786+Arisha27@users.noreply.github.com>
2022-08-02 20:14:39 +0500

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const connect = async () => {
         version
     }
     const killua = new WAConnection(WASocket(connOptions))
-    if (config.APIs.apikey == "YOURAPIKEY") {
+    if (config.APIs.apikey == "c719318f84") {
         console.log(chalk.black(chalk.bgRedBright('Apikey is not valid, please check at config.json')))
         process.exit();
     }
